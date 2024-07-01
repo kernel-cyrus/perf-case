43dd85317dd35a7adc0309f9a8b58c4612b3b75d
Cyrus Huang <cyrus.kernel@gmail.com>
2024-07-01 21:01:06 +0800

    update test framework

--- perf_test.c
+++ perf_test.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <sys/mman.h>
 #include "perf_test.h"
 #include "perf_event.h"
@@ -71,22 +72,54 @@ int perf_test_intcal(struct perf_event *events, int event_num)
 	return 0;
 }
 
-int main()
+void print_help()
+{
+	int i;
+	int test_num = sizeof(g_tests) / sizeof(struct perf_test);
+
+	printf("./perf_test <test>\n");
+	printf("==========================\n");
+	for (i = 0; i < test_num; i++)
+		printf("%s - %s\n", g_tests[i].name, g_tests[i].desc);
+}
+
+int run_test(char *test_name)
 {
 	int i, ret;
 	struct perf_test *p_test;
 	int test_num = sizeof(g_tests) / sizeof(struct perf_test);
 	int event_num = sizeof(g_events) / sizeof(struct perf_event);
+	bool found = false;
 
 	for (i = 0; i < test_num; i++) {
 		p_test = &g_tests[i];
-		printf("%s\n", p_test->name);
-		printf("=======================\n");
-		printf("%s\n\n", p_test->desc);
-		ret = p_test->func(g_events, event_num);
-		if (ret < 0)
-			return ret;
+		if (!strcmp(test_name, p_test->name) || !strcmp(test_name, "all")) {
+			found = true;
+			printf("%s\n", p_test->name);
+			printf("=======================\n");
+			printf("%s\n\n", p_test->desc);
+			ret = p_test->func(g_events, event_num);
+			if (ret < 0)
+				return ret;
+		}
 	}
+
+	if (!found) {
+		printf("ERROR: No test named \"%s\"\n", test_name);
+		exit(0);
+	}
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc < 2) {
+		print_help();
+		return 0;
+	}
+
+	run_test(argv[1]);
 
 	return 0;
 }
